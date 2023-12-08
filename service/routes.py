061d8948ac6a651efce35be3e8212fcd11a53259
wolfgangjblack <wolfgangjblack@gmail.com>
2023-12-07 19:40:13 -0500

    fixed indent error

--- service/routes.py
+++ service/routes.py
@@ -95,7 +95,7 @@ def get_accounts(account):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
+def update_accounts(account_id):
         """
         Update an Account
         This endpoint will update an Account based on the posted data
